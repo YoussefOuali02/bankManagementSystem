65c8f9c3bf9b086441ff60210f34d7eed743f89f
Youssef Ouali <you.oua.16@gmail.com>
2025-04-26 21:57:46 +0100

    Initial Commit

---
+++ src/Login.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    Login(){
+        setVisible(true);
+    }
+
+    public static void main (String[] args){
+        new Login();
+    }
+}
